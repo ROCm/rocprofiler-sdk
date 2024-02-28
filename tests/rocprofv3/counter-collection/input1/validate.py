002cd8afaf035ca0ab3d3534d59075c088ae0770
Gopesh Bhardwaj <gopesh.bhardwaj@amd.com>
2024-02-29 04:38:28 +0530

    counter collection multi-kernel test (#492)
    * counter collection multi-kernel test
    
    * Adding counter collection in rocprofv3 script
    
    [ROCm/rocprofiler-sdk commit: 31dcfabe2324f2881551ec183f8cab9d5181fe4e]

--- tests/rocprofv3/counter-collection/input1/validate.py
+++ tests/rocprofv3/counter-collection/input1/validate.py
@@ -2,15 +2,29 @@ import pandas as pd
 import sys
 import pytest
 
+kernel_list = ["addition_kernel", "subtract_kernel", "multiply_kernel", "divide_kernel"]
+
 
 def test_validate_counter_collection_pmc1(input_data: pd.DataFrame):
     df = input_data
 
     assert df.empty == False
-    assert df["Agent_Id"].map(type).eq(int).all()
+    assert (df["Agent_Id"].astype(int).values > 0).all()
+    assert (df["Queue_Id"].astype(int).values > 0).all()
+    assert (df["Process_Id"].astype(int).values > 0).all()
     assert len(df["Kernel-Name"]) > 0
-    assert df["Kernel-Name"].str.contains("matrixTranspose").any()
+    df_list = df["Kernel-Name"].values.flatten().tolist()
+    # Check if each string in kernel_list is present at least once
+    missing_kernels = []
+    for kernel in kernel_list:
+        if kernel not in df_list:
+            missing_kernels.append(kernel)
+
+    assert (
+        not missing_kernels
+    ), f"The following kernel names are missing from the out file: {missing_kernels}"
     assert df["Counter_Name"].str.contains("SQ_WAVES").all()
+    assert len(df["Counter_Value"]) > 0
 
 
 if __name__ == "__main__":
