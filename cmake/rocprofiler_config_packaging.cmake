5d365bb8b8616ee17f0401fffb5b6644e00c2554
Jonathan R. Madsen <jrmadsen@users.noreply.github.com>
2024-04-24 01:19:32 -0500

    Fix LICENSE install location + CPACK_RPM_PACKAGE_LICENSE (#820)
    - LICENSE needs to be installed in share/doc/rocprofiler-sdk not share/doc/rocprofiler
    - CPACK_RPM_PACKAGE_LICENSE should be set to license type not the license location

--- cmake/rocprofiler_config_packaging.cmake
+++ cmake/rocprofiler_config_packaging.cmake
@@ -122,12 +122,6 @@ foreach(COMPONENT_GROUP ${ROCPROFILER_COMPONENT_GROUPS})
     endforeach()
 endforeach()
 
-# Install license file
-install(
-    FILES ${CPACK_RESOURCE_FILE_LICENSE}
-    DESTINATION ${CMAKE_INSTALL_DOCDIR}
-    COMPONENT core)
-
 # -------------------------------------------------------------------------------------- #
 #
 # Debian package specific variables
@@ -196,7 +190,7 @@ if(CPACK_RPM_PACKAGE_RELEASE)
     set(CPACK_RPM_PACKAGE_RELEASE_DIST ON)
 endif()
 set(CPACK_RPM_FILE_NAME "RPM-DEFAULT")
-set(CPACK_RPM_PACKAGE_LICENSE "${PROJECT_SOURCE_DIR}/LICENSE")
+set(CPACK_RPM_PACKAGE_LICENSE "MIT")
 
 # -------------------------------------------------------------------------------------- #
 #
