95ac07d24b2e23d2bc9443d2c223aac0fc457077
Ammar ELWazir <ammar.elwazir@amd.com>
2025-08-08 04:02:17 +0000

    Changing CDash Project
    * Changing CDash Project
    
    * Fixing CI
    
    * Fixing AQLProfile CDash
    
    * Fixing CI
    
    * Fixing CI
    
    * Fixing CI
    
    * Fixing CI
    
    * Fixing CI
    
    * Fixing CI
    
    * Fixing CI
    
    * Fixing CI
    
    * Fixing CI
    
    * Fixing CI
    
    * Fixing CI
    
    * Fixing CI
    
    * Fixing CI
    
    * Fixing CI
    
    * Fixing CI
    
    * Fixing CI
    
    * Fixing CI
    
    * Fixing CI
    
    * Fixing CI
    [rocm-systems] ROCm/rocm-systems#188 (commit 906030c)

--- source/scripts/upload-image-to-github.py
+++ source/scripts/upload-image-to-github.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
         "--repo-url",
         help="Base GitHub repo URL",
         type=str,
-        default="https://github.com/AMD-ROCm-Internal/rocprofiler-sdk-internal",
+        default="https://github.com/ROCm/rocm-systems",
     )
     parser.add_argument(
         "-o",
