b17ac4d4ba11e7215db53f6ff5d170da1512be54
Vladimir Indic <139573562+vlaindic@users.noreply.github.com>
2025-10-22 14:18:08 +0200

    PCS: Temporarily Masking Trap Handler Latency (#1109)
    - Temporarily, masking out the trap handler latency, by detecting
    untagged error samples.
    - Disabling checks for the number of invalid samples.

--- source/lib/rocprofiler-sdk/pc_sampling/parser/correlation.hpp
+++ source/lib/rocprofiler-sdk/pc_sampling/parser/correlation.hpp
@@ -260,6 +260,14 @@ add_upcoming_samples(const device_handle     device,
             auto                          dispatch_correlation_ids = corr_map->get(device, trap);
             pc_sample.dispatch_id    = dispatch_correlation_ids.dispatch_id;
             pc_sample.correlation_id = dispatch_correlation_ids.correlation_id;
+
+            if(pc_sample.pc.code_object_id == ROCPROFILER_CODE_OBJECT_ID_NONE)
+            {
+                // We observed an error sample, that was not being
+                // tagged with the error bit on time due to high latency in the trap handler.
+                // Thus, we are declaring the sample invalid, by setting its size to zero.
+                pc_sample.size = 0;
+            }
         } catch(std::exception& e)
         {
             // TODO: introduce ROCPROFILER_DISPATCH_ID_INTERNAL_NONE
